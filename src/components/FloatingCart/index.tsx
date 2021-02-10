f13163e83601db89d82db8b1115e7e865510676d
Lucas <lucasfarias.bsb@hotmail.com>
2021-02-09 21:17:37 -0300

    Finished

--- src/components/FloatingCart/index.tsx
+++ src/components/FloatingCart/index.tsx
@@ -25,14 +25,19 @@ const FloatingCart: React.FC = () => {
 
   const cartTotal = useMemo(() => {
     // TODO RETURN THE SUM OF THE PRICE FROM ALL ITEMS IN THE CART
-
-    return formatValue(0);
+    const total = products.reduce((acc, product) => {
+      const subTotal = product.price * product.quantity;
+      return subTotal + acc;
+    }, 0);
+    return formatValue(total);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
     // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
-
-    return 0;
+    const totalItens = products.reduce((acc, product) => {
+      return acc + product.quantity;
+    }, 0);
+    return totalItens;
   }, [products]);
 
   return (
